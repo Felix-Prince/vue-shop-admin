449309aada98adf9bfc4fd70a9fac5845364745d
Felix-Prince <zhupengfei1995@gmail.com>
2019-06-27 19:43:48 +0800

    项目解构搭建完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+// import store from "./store";
+import ElementUI from "element-ui";
+
+// 引入elementUI的CSS样式
+import "element-ui/lib/theme-chalk/index.css";
+
+// 把引入的ElementUI添加到Vue上
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
+  // store,
   render: h => h(App)
 }).$mount("#app");
